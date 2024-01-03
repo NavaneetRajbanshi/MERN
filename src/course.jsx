a3873f52a5be4539cff0d32d9743a89794cec90a
NavneetRajbanshi <navneetrajbanshi85@gmail.com>
2024-01-03 22:45:02 +0545

    course

--- src/course.jsx
+++ src/course.jsx
@@ -6,6 +6,13 @@ const App = () => {
         </div>
     )
 }
-
+const App = () => {
+    console.log('HELLO from components')
+    return (
+        <div>
+            <p>hello world</p>
+        </div>
+    )
+}
 export default App 
   
